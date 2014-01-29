63e1ed827c49c63adc5007029c42a5519d5a652e
Luis Doubrava <luis@cg.nl>
2014-01-29 09:25:54 +0100

    fix anayzer for better request_duration calculation

--- lib/cg_request_monitor/middleware/analyze_request_monitor.rb
+++ lib/cg_request_monitor/middleware/analyze_request_monitor.rb
@@ -6,9 +6,9 @@ module CgRequestMonitor
       end
 
       def call(env)
-        request_duration = CgRequestMonitor.request_actions.inject(0){|total_request_duration, action| total_request_duration += action[:duration]; total_request_duration}
-        if request_duration >= (CgRequestMonitor.request_length * 1000)
-          CgRequestMonitor::Mailer.slow_request_email(CgRequestMonitor.request_actions, (request_duration / 1000)).deliver
+        request_duration = Time.now - CgRequestMonitor.start
+        if request_duration >= CgRequestMonitor.request_length && CgRequestMonitor.send_email
+          CgRequestMonitor::Mailer.slow_request_email(CgRequestMonitor.request_actions, request_duration).deliver
         end
         CgRequestMonitor.request_actions = []
         @app.call(env)
