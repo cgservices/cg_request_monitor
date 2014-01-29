b346b25ab4b371310f85612c5337e89e38427a91
Luis Doubrava <luis@cg.nl>
2014-01-29 09:25:07 +0100

    fix calculation of request time

--- lib/cg_request_monitor.rb
+++ lib/cg_request_monitor.rb
@@ -3,11 +3,15 @@ require 'cg_request_monitor/mailer'
 require 'cg_request_monitor/railtie' if defined?(Rails)
 
 module CgRequestMonitor
-  mattr_accessor :insert_before, :insert_after, :request_length, :monitor_email_address
-  mattr_accessor :request_actions
+  mattr_accessor :insert_before, :insert_after, :request_length, :monitor_email_address, :send_email
+  mattr_accessor :request_actions, :start
 
   def self.request_length
-    @@request_length ||= 10
+    @@request_length ||= 60
+  end
+
+  def self.send_email
+    @@send_email ||= true
   end
 
   def self.request_actions
@@ -15,11 +19,12 @@ module CgRequestMonitor
   end
 
   def self.monitor_email_address
-    @@monitor_email_address ||= 'developers@cg.nl'
+    @@monitor_email_address ||= "your.email@address.com"
   end
 end
 
 ActiveSupport::Notifications.subscribe do |name, start, finish, id, payload|
-  duration = (finish - start) * 1000
-  CgRequestMonitor.request_actions << {name: name, duration: duration, payload: payload}
+  duration = (finish - start)
+  CgRequestMonitor.start = start if CgRequestMonitor.request_actions.empty?
+  CgRequestMonitor.request_actions << {name: name, duration: duration, payload: payload, start: start, finish: finish}
 end
