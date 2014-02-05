ea916d44cd8891269ba2d627cc6d3e90dd366805
Luis Doubrava <luis@cg.nl>
2014-02-05 10:38:36 +0100

    cleanup railtie

--- lib/cg_request_monitor/railtie.rb
+++ lib/cg_request_monitor/railtie.rb
@@ -3,7 +3,7 @@ require 'cg_request_monitor/middleware/analyze_request_monitor'
 module CgRequestMonitor
   class CgRequestMonitorRailtie < Rails::Railtie
     initializer "cg_request_monitor_railtie.configure_rails_initialization" do |app|
-      app.middleware.use CgRequestMonitor::Middleware::AnalyzeRequestMonitor, after: Warden::Manager
+      app.middleware.use CgRequestMonitor::Middleware::AnalyzeRequestMonitor
     end
   end
 end
