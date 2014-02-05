5d6241176c6df8d175648e71a82deb0de39a511e
Luis Doubrava <luis@cg.nl>
2014-02-05 10:38:11 +0100

    add request url to middleware

--- lib/cg_request_monitor/middleware/analyze_request_monitor.rb
+++ lib/cg_request_monitor/middleware/analyze_request_monitor.rb
@@ -6,11 +6,17 @@ module CgRequestMonitor
       end
 
       def call(env)
-        request_duration = (Time.now - CgRequestMonitor.start).round(3)
-        if request_duration >= CgRequestMonitor.request_length && CgRequestMonitor.send_email
-          CgRequestMonitor::Mailer.slow_request_email(CgRequestMonitor.request_actions, request_duration).deliver
+        request = ActionDispatch::Request.new(env)
+        parsed_uri = URI.parse(request.original_url)
+
+        if CgRequestMonitor.request_actions.any?
+          request_duration = (Time.now - CgRequestMonitor.start).round(3)
+          if request_duration >= CgRequestMonitor.request_length && CgRequestMonitor.send_email
+            notifications = CgRequestMonitor.request_actions.sort_by{|action| (action[:start] - CgRequestMonitor.start)}
+            CgRequestMonitor::Mailer.slow_request_email(notifications, request_duration, parsed_uri).deliver
+          end
+          CgRequestMonitor.request_actions = []
         end
-        CgRequestMonitor.request_actions = []
         @app.call(env)
       end
     end
